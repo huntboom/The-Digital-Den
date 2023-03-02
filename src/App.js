b0e8a75f99071d557a0740440c4646d8402a5895
huntboom <hkarsseboom@hotmail.com>
2023-03-02 09:43:57 -0500

    Adding camera movements, added camera state logger for testing purposes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import BookFinder from './BookFinder';
 import Library from './OpenLibrary.js';
 import Header from './Header';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { Suspense } from 'react';
+import { useState, Suspense } from 'react';
 import Shelf from './Shelf.js';
 import Book3 from './Book3.js';
 import About from './About.js';
@@ -17,7 +17,16 @@ import store from './store';
 import SearchBooks from './SearchBooks.js';
 import Book2 from './Book2.js';
 import Table from './Table.js';
+
 function App() {
+  const controlsRef = useRef();
+  const handleButtonClick = () => {
+    console.log(controlsRef.current.object.position);
+  };
+  const handleCamera = () => {
+    setCameraPosition([9.33161480825913, 0.3292314177365565, 12.375517658943455]);
+  };
+  const [cameraPosition, setCameraPosition] = useState([0, 0.3, 4.5]);
   return (
     <Provider store={store}>
       <div className="bg-image">
@@ -26,16 +35,18 @@ function App() {
         <SearchBooks />
         <Library />
         <GenreList />
+        <button onClick={handleButtonClick}>Log Camera Position</button>
+        <button onClick={handleCamera}>Move Camera</button>
         <div className="readingroom">
-          <Canvas camera={{ position: [0, 0.3, 4.5] }}>
+          <Canvas camera={{ position: cameraPosition }}>
             <group position={[0, -0.4, 0.5]}>
-              <OrbitControls />
+              <OrbitControls ref={controlsRef} />
               <Shelf position={[0, 0, 3.25]} scale={[5, 1, 1]} />
               <ambientLight />
               <Suspense>
                 <CompleteShelf position={[-0.5, 0.7, 2.4]} />
                 <Table scale={1} position={[10, -5, 5]} />
-                <Book2 scale={1} rotation={[Math.PI / 2, Math.PI / 2, 0]} position={[10, -1, 5]} />
+                <Book2 scale={1} rotation={[Math.PI / 2, 0, 0]} position={[10, -1.777, 5]} />
                 {/* <Book3 scale={0.02} position={[0, 0.7, 3.4]} /> */}
                 {/* <Book3 scale={0.02} position={[0.04, 0.7, 3.4]} /> */}
                 {/* <Book3 scale={0.02} position={[0.08, 0.7, 3.4]} /> */}
