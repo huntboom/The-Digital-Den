7fadf6e5f32a4e6da4c9541ba05b36f5a989cafc
huntboom <hkarsseboom@hotmail.com>
2024-02-09 15:57:29 -0500

    Fixed header component to scroll with rest of app

--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,29 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: 'black',
+    position: 'fixed', // Set position to fixed
+    width: '100%', // Make it take the full width
+    zIndex: theme.zIndex.appBar + 1, // Ensure it's above other elements
   },
   title: {
     fontFamily: '"Palatino Linotype", "Book Antiqua", Palatino, serif',
     flexGrow: 1,
   },
+  toolbar: {
+    justifyContent: 'center', // Center the content horizontally
+  },
 }));
 
 const Header = () => {
   const classes = useStyles();
+
   return (
     <AppBar position="static" className={classes.root}>
-      <Toolbar>
+      <Toolbar className={classes.toolbar}>
         <Typography variant="h6" className={classes.title}>
           The Digital Den
         </Typography>
