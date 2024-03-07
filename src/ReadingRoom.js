b702b7806c7cc922cc8210c8bc3f23a4e692ea11
huntboom <hkarsseboom@hotmail.com>
2024-03-07 16:50:48 -0500

    camera now dollys left and right and stops when users stop hovering for an intuitive experience

--- src/ReadingRoom.js
+++ src/ReadingRoom.js
@@ -14,6 +14,7 @@ const ReadingRoom = () => {
   const controlsRef = useRef();
   const cameraControlRef = useRef(null);
   const [isTablePosition, setIsTablePosition] = useState(false);
+  const [moveIntervalId, setMoveIntervalId] = useState(null); // State for interval tracking
 
   const handleTablePosition = () => {
     setIsTablePosition(true);
@@ -26,47 +27,57 @@ const ReadingRoom = () => {
   const handleButtonClick = () => {
     const arr1 = cameraControlRef.current._camera.position.toArray();
     const arr2 = cameraControlRef.current._target.toArray();
-    const position = [...arr1,...arr2];
+    const position = [...arr1, ...arr2];
     console.log(position);
   };
 
-  const dollyCameraLeft = () => {
-          const cameraPosition = cameraControlRef.current._camera.position.toArray();
-          const targetPosition = cameraControlRef.current._target.toArray();
-          const newXCameraPosition = cameraPosition[0] - 0.5;
-          const newXTargetPosition = targetPosition[0] - 0.5;
-          cameraControlRef.current?.setLookAt(
-            newXCameraPosition, cameraPosition[1], cameraPosition[2], // new camera position
-            newXTargetPosition, targetPosition[1], targetPosition[2], // new target position
-            true
-          );
-          handleStartingPosition();
-        };
-const dollyCameraRight = () => {
-          const cameraPosition = cameraControlRef.current._camera.position.toArray();
-          const targetPosition = cameraControlRef.current._target.toArray();
-          const newXCameraPosition = cameraPosition[0] + 0.5;
-          const newXTargetPosition = targetPosition[0] + 0.5;
-          cameraControlRef.current?.setLookAt(
-            newXCameraPosition, cameraPosition[1], cameraPosition[2], // new camera position
-            newXTargetPosition, targetPosition[1], targetPosition[2], // new target position
-            true
-          );
-          handleStartingPosition();
-        };
+  const dollyCameraStop = () => {
+    if (moveIntervalId) {
+      clearInterval(moveIntervalId);
+      setMoveIntervalId(null);
+    }
+  };
 
-function HTMLInside() {
-  return(
-        <div className='bounds'>
-        <div className='overlayed'
-             onPointerOver={() =>{
-            dollyCameraLeft();
-            }} 
-              >
-          </div>
-        </div>
-  );
-}
+  const dollyCameraLeft = () => {
+    const moveAmount = 0.5;
+
+    const moveCamera = () => {
+      const cameraPosition = cameraControlRef.current._camera.position.toArray();
+      const targetPosition = cameraControlRef.current._target.toArray();
+      const newXCameraPosition = cameraPosition[0] - moveAmount;
+      const newXTargetPosition = targetPosition[0] - moveAmount;
+      cameraControlRef.current?.setLookAt(
+        newXCameraPosition, cameraPosition[1], cameraPosition[2], // new camera position
+        newXTargetPosition, targetPosition[1], targetPosition[2], // new target position
+        true
+      );
+    };
+
+    dollyCameraStop(); // Clear any existing interval 
+    const intervalId = setInterval(moveCamera, 50);
+    setMoveIntervalId(intervalId);
+  };
+
+  const dollyCameraRight = () => {
+    const moveAmount = 0.5;
+
+    const moveCamera = () => {
+      const cameraPosition = cameraControlRef.current._camera.position.toArray();
+      const targetPosition = cameraControlRef.current._target.toArray();
+      const newXCameraPosition = cameraPosition[0] + moveAmount;
+      const newXTargetPosition = targetPosition[0] + moveAmount;
+      cameraControlRef.current?.setLookAt(
+        newXCameraPosition, cameraPosition[1], cameraPosition[2], 
+        newXTargetPosition, targetPosition[1], targetPosition[2], 
+        true
+      );
+    };
+
+    dollyCameraStop(); 
+    const intervalId = setInterval(moveCamera, 50);
+    setMoveIntervalId(intervalId);
+  };
+
   return (
     <div>
       <button
@@ -95,14 +106,14 @@ function HTMLInside() {
             true
           );
           handleStartingPosition();
-          }}
+        }}
       >
         Move 5 Right
       </button>
       <button
         type="button"
         onClick={() => {
-         // Extract current camera position and target as arrays
+          // Extract current camera position and target as arrays
           const cameraPosition = cameraControlRef.current._camera.position.toArray();
           const targetPosition = cameraControlRef.current._target.toArray();
           // Calculate new positions by adding 0.5 to the X coordinates for moving right
@@ -130,24 +141,32 @@ function HTMLInside() {
         Go to Table Position
       </button>
       <button onClick={handleButtonClick}>Log Camera Position</button>
-    <div className="homepage">
-    <div className="leftSection" onPointerOver={() =>{dollyCameraLeft()}}></div>
-    <div className="rightSection" onPointerOver={() =>{dollyCameraRight()}}></div>
-    <div className="readingroom">
-      <Canvas>
-        <CameraControls ref={cameraControlRef} smoothTime={0.8} />
-        <group position={[0, -0.6, 1.2]}>
-          <Shelf position={[0, 0, 3.25]} scale={[5, 1, 1]} />
-          <ambientLight />
-          <Suspense>
-            <CompleteShelf position={[-1.7, 0.7, 2.2]} />
-            <Table scale={1} position={[10, -5, 5]} />
-            <Book2 isTablePosition={isTablePosition} scale={1} rotation={[Math.PI / 2, 0, 0]} position={[10, -1.777, 5]} />
-          </Suspense>
-        </group>
-      </Canvas>
-    </div>
-    </div>
+      <div className="homepage">
+        <div 
+          className="leftSection" 
+          onPointerOver={() => dollyCameraLeft()} 
+          onPointerOut={() => dollyCameraStop()}
+        ></div>
+        <div 
+          className="rightSection" 
+          onPointerOver={() => dollyCameraRight()} 
+          onPointerOut={() => dollyCameraStop()}
+        ></div>
+        <div className="readingroom">
+          <Canvas>
+            <CameraControls ref={cameraControlRef} smoothTime={0.8} />
+            <group position={[0, -0.6, 1.2]}>
+              <Shelf position={[0, 0, 3.25]} scale={[5, 1, 1]} />
+              <ambientLight />
+              <Suspense>
+                <CompleteShelf position={[-1.7, 0.7, 2.2]} />
+                <Table scale={1} position={[10, -5, 5]} />
+                <Book2 isTablePosition={isTablePosition} scale={1} rotation={[Math.PI / 2, 0, 0]} position={[10, -1.777, 5]} />
+              </Suspense>
+            </group>
+          </Canvas>
+        </div>
+      </div>
     </div>
   );
 };
